6b8a825b6a815e924611b4f2b87f52c5f884f04e
Alon Gross <alongross75@gmail.com>
2024-12-06 12:03:56 +0200

    first commit

---
+++ node_modules/@synthetixio/synpress-cache/types/prepareExtension.d.ts
@@ -0,0 +1,4 @@
+export declare const DEFAULT_METAMASK_VERSION = "11.9.1";
+export declare const EXTENSION_DOWNLOAD_URL = "https://github.com/MetaMask/metamask-extension/releases/download/v11.9.1/metamask-chrome-11.9.1.zip";
+export declare function prepareExtension(): Promise<string>;
+//# sourceMappingURL=prepareExtension.d.ts.map
